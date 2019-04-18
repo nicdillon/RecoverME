3d434e54125dede16ac0529b27f00fe1ce2af071
Nic Dillon <nidillon@davidson.edu>
2019-04-18 19:35:29 -0400

    Update 4/18/19

---
+++ RecoverME/Workout.swift
@@ -0,0 +1,22 @@
+//
+//  Workout.swift
+//  RecoverME
+//
+//  Created by Nic Dillon on 4/18/19.
+//  Copyright © 2019 Nic Dillon. All rights reserved.
+//
+
+import Foundation
+
+class Workout {
+    
+    //MARK: Atributes
+    var time = 0
+    var calories = 0
+    var type: String?
+    
+    //MARK: Actions
+    
+    
+    
+}
