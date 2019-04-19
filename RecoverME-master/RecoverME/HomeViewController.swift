a66fcf92d5cadfc1bbb5499755218c05c1d40c4d
Conor Huh <48928653+ConorHuh@users.noreply.github.com>
2019-04-18 21:05:26 -0400

    Add files via upload

---
+++ RecoverME-master/RecoverME/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  RecoverME
+//
+//  Created by Nic Dillon on 4/18/19.
+//  Copyright © 2019 Nic Dillon. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    
+    //MARK: Attributes
+    var height = 0
+    var weight = 0
+    var goal = 0
+    var date = 0
+    var email: String?
+    var password: String?
+    //MARK: Actions
+    
+    
+}
+
+
